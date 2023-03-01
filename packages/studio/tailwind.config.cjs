1353b3b38b4b5aa3a5f06e11084ff284aa8a3368
Gustav Hansen <gustav.hansen@strapi.io>
2023-03-01 12:43:22 +0100

    complete tw

---
+++ packages/studio/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/@unding/ui/src/**/*.{js,svelte,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
